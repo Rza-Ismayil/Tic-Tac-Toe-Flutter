fad9d93e581d43c0de71c2b23550d17bf118a194
Rza-Ismayil <rzaismail82@gmail.com>
2022-02-09 16:24:46 +0400

    done

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String winnerData = '';
-
+  String turn = 'X';
+  bool gameOver = false;
   List<List<String>> board = [
     ['', '', ''],
-    ['', 'X', ''],
-    ['', 'O', '']
+    ['', '', ''],
+    ['', '', '']
   ];
 
   Widget genBtn(int x, int y) {
@@ -35,13 +36,70 @@ class _MyAppState extends State<MyApp> {
       height: 80.0,
       width: 80.0,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            pressed(x, y);
+          });
+        },
         child: Text(
           board[x][y],
           style: const TextStyle(fontSize: 40.0),
         ),
       ),
     );
+  }
+
+  void pressed(int x, int y) {
+    if (board[x][y] == '' && !gameOver) {
+      board[x][y] = turn;
+      checkForWin();
+      changeTurn();
+    }
+  }
+
+  void changeTurn() {
+    if (turn == 'X') {
+      turn = 'O';
+    } else {
+      turn = 'X';
+    }
+  }
+
+  void checkForWin() {
+    if ((board[0][0] == turn && board[0][1] == turn && board[0][2] == turn) ||
+        (board[1][0] == turn && board[1][1] == turn && board[1][2] == turn) ||
+        (board[2][0] == turn && board[2][1] == turn && board[2][2] == turn) ||
+        (board[0][0] == turn && board[1][0] == turn && board[2][0] == turn) ||
+        (board[0][1] == turn && board[1][1] == turn && board[2][1] == turn) ||
+        (board[0][2] == turn && board[1][2] == turn && board[2][2] == turn) ||
+        (board[0][0] == turn && board[1][1] == turn && board[2][2] == turn) ||
+        (board[2][0] == turn && board[1][1] == turn && board[0][2] == turn)) {
+      gameOver = true;
+      winnerData = turn + ' WINS';
+      return;
+    }
+    bool isTie = true;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (board[i][j] == '') {
+          isTie = false;
+        }
+      }
+    }
+    if (isTie) {
+      gameOver = true;
+      winnerData = 'Tie';
+    }
+  }
+
+  void reset() {
+    gameOver = false;
+    winnerData = '';
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        board[i][j] = '';
+      }
+    }
   }
 
   @override
@@ -81,7 +139,11 @@ class _MyAppState extends State<MyApp> {
             child: Text(winnerData),
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              setState(() {
+                reset();
+              });
+            },
             child: const Text('restart'),
           )
         ],
